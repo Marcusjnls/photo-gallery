17dd6e4d00cbc2d386f0e8fd41820ad403a41245
Marcus Jean-Louis <marcusjnls@gmail.com>
2019-10-04 15:39:58 +0300

    Add configurations for static files

--- gallery/settings.py
+++ gallery/settings.py
@@ -15,6 +15,19 @@ import django_heroku
 import dj_database_url
 from decouple import config,Csv
 
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.9/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+
 # SECURITY WARNING: don't run with debug turned on in production!
 MODE=config("MODE", default="dev")
 SECRET_KEY = config('SECRET_KEY')
