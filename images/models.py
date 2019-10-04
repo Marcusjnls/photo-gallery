a63ecc3166e6accddab5e363d9f267a2edf2d144
Marcus Jean-Louis <marcusjnls@gmail.com>
2019-10-04 14:48:22 +0300

    Add Location model

--- images/models.py
+++ images/models.py
@@ -4,6 +4,11 @@ from django.contrib.auth.models import User
 from tinymce.models import HTMLField
 
 # Create your models here.
+class Location(models.Model):
+    name=models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
 class Image(models.Model):
     name= models.CharField(max_length=50)
     description = HTMLField()
