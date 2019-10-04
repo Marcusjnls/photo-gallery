e8353669c502ec4028fef6ea68ceb7c29571964a
Marcus Jean-Louis <marcusjnls@gmail.com>
2019-10-04 12:44:04 +0300

    Add image model

--- images/models.py
+++ images/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+import datetime as dt
+from django.contrib.auth.models import User
+from tinymce.models import HTMLField
 
 # Create your models here.
+class Image(models.Model):
+    name= models.CharField(max_length=50)
+    description = HTMLField()
+    gallery_image = models.ImageField(upload_to='picha/', blank=True)
+    categories = models.ManyToManyField(categories)
+    location = models.ForeignKey(Location)
